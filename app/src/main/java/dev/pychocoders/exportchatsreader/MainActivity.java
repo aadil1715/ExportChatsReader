10631b63a8e48582242c75ec58f323df5c61bcb7
aadil1715 <aadilshaikh862@gmail.com>
2019-04-02 08:36:59 +0530

    Reads whole chat line by line

--- app/src/main/java/dev/pychocoders/exportchatsreader/MainActivity.java
+++ app/src/main/java/dev/pychocoders/exportchatsreader/MainActivity.java
@@ -1,12 +1,16 @@
 package dev.pychocoders.exportchatsreader;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -33,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 i.addCategory(Intent.CATEGORY_OPENABLE);
                 i.setType("text/*");
-                startActivity(i);
+
+                startActivityForResult(i, 123);
             }
         });
 
@@ -52,25 +57,61 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-                BufferedReader br = null;
                 try {
-                    br = new BufferedReader(new FileReader(chat));
+                    BufferedReader fr = new BufferedReader(new FileReader(chat));
+                    StringBuilder st = new StringBuilder(100000);
+                    String line;
+                    while ((line = fr.readLine()) != null){
+                        st.append(line).append("\n");
+                        line = fr.readLine();
+                    }
+                    Intent i;
+                    i = new Intent(MainActivity.this,Main2Activity.class);
+                    i.putExtra("data",st.toString());
+                    startActivity(i);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
-                }
-
-                String st = new String();
-                try {
-                    st = br.readLine();
-                } catch (Exception e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-                Intent i;
-                i = new Intent(MainActivity.this,Main2Activity.class);
-                i.putExtra("data",st);
-                startActivity(i);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 123 && resultCode == Activity.RESULT_OK){
+            Uri uri;
+            if(data != null){
+                uri = data.getData();
+                launchFile(uri.getPath());
+            }
+        }
+    }
+
+    private void launchFile(String path) {
+        String[] paths = path.split(":");
+        File chat = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + paths[1]);
+        try {
+            BufferedReader fr = new BufferedReader(new FileReader(chat));
+            StringBuilder st = new StringBuilder(1000000000);
+            String line;
+            while ((line = fr.readLine()) != null){
+                st.append(line).append("\n");
+                line = fr.readLine();
+            }
+            Intent i;
+            i = new Intent(MainActivity.this, Main2Activity.class);
+            i.putExtra("data", st.toString());
+            startActivity(i);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
