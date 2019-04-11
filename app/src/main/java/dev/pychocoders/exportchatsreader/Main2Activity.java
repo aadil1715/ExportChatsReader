826f8aef14e5c8bc95fff117afdae8a17a96a28a
aadil1715 <aadilshaikh862@gmail.com>
2019-04-11 21:04:10 +0530

    displays in listview

--- app/src/main/java/dev/pychocoders/exportchatsreader/Main2Activity.java
+++ app/src/main/java/dev/pychocoders/exportchatsreader/Main2Activity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends Activity {
 
@@ -12,9 +16,12 @@ public class Main2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        TextView tv = findViewById(R.id.textView);
+       ListView lv = (ListView) findViewById(R.id.listview);
         Intent inet = getIntent();
         String chat = (String) inet.getSerializableExtra("data");
-        tv.setText(chat);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(chat);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        lv.setAdapter(adapter);
     }
 }
